8b0044c0873d736a0cc54c7f2d8ea37133e504ac
sumit9868 <sumitsaurabh9868@gmail.com>
2020-05-16 17:40:41 +0530

    Assignment7_Part(A)andPart(B)

--- assignment 7/2018UCO1660_Assignment7_Part(A).cpp
+++ assignment 7/2018UCO1660_Assignment7_Part(A).cpp
@@ -8,38 +8,38 @@ using namespace std;
 
 int main()
 {
-    int sndrFDS1[2],sndrFDS2[2],AKCFDS1[2],AKCFDS2[2];
+    int sndrFDS1[2],sndrFDS2[2],ACKFDS1[2],ACKFDS2[2];
     int rtrnStatus1, rtrnStatus2,rtrnStatus3,rtrnStatus4;
 
     rtrnStatus1 = pipe(sndrFDS1);
     if (rtrnStatus1 == -1)
     {
-        cout<<"Error in crating pipe 1 \n";
+        cout<<"Error in creating pipe 1 \n";
         return 1;
     }
 
     rtrnStatus2 = pipe(sndrFDS2);
     if (rtrnStatus2 == -1)
     {
-        cout<<"Error in crating pipe 2 \n";
+        cout<<"Error in creating pipe 2 \n";
         return 1;
     }
 
-    rtrnStatus3=pipe(AKCFDS1);
+    rtrnStatus3=pipe(ACKFDS1);
     if(rtrnStatus3==-1)
     {
-        cout<<"Error in crating pipe 3\n";
+        cout<<"Error in creating pipe 3\n";
         return 1;
     }
 
-    rtrnStatus4=pipe(AKCFDS2);
+    rtrnStatus4=pipe(ACKFDS2);
     if(rtrnStatus4==-1)
     {
-        cout<<"Error in crating pipe 4\n";
+        cout<<"Error in creating pipe 4\n";
         return 1;
     }
 
-    int nonblockstatus=fcntl(AKCFDS2[0], F_SETFL, O_NONBLOCK);
+    int nonblockstatus=fcntl(ACKFDS2[0], F_SETFL, O_NONBLOCK);
     if(nonblockstatus<0){cout<<"Error";return 1;}
 
     int n1 = fork();
@@ -49,8 +49,8 @@ int main()
         char readmsg[20];
         close(sndrFDS1[0]);
         close(sndrFDS2[0]);close(sndrFDS2[1]);
-        close(AKCFDS1[0]);close(AKCFDS1[1]);
-        close(AKCFDS2[1]);
+        close(ACKFDS1[0]);close(ACKFDS1[1]);
+        close(ACKFDS2[1]);
         while(true)
         {
             cout<<"Sending the message from sender....\n";
@@ -60,7 +60,7 @@ int main()
             for(int i=0;i<8;i++)
             {
                 sleep(1);
-                if(read(AKCFDS2[0],readmsg,sizeof(readmsg))!=-1){res=true;break;}
+                if(read(ACKFDS2[0],readmsg,sizeof(readmsg))!=-1){res=true;break;}
             }
             if(res==true){cout<<"Acknowledged\n";}
             else{cout<<"Resending message form the reciever\n";}
@@ -72,8 +72,8 @@ int main()
         char readmsg[20];
         close(sndrFDS1[1]);
         close(sndrFDS2[0]);
-        close(AKCFDS1[1]);
-        close(AKCFDS2[0]);
+        close(ACKFDS1[1]);
+        close(ACKFDS2[0]);
         srand(time(0));
         while(true)
         {
@@ -87,7 +87,7 @@ int main()
             cout<<"Message in channel transmitting message to receiver"<<endl;
             sleep(1);
             write(sndrFDS2[1],readmsg,sizeof(readmsg));
-            read(AKCFDS1[0],readmsg,sizeof(readmsg));
+            read(ACKFDS1[0],readmsg,sizeof(readmsg));
             num=rand()%100+1;
             if(num<30)
             {
@@ -96,7 +96,7 @@ int main()
             }
             cout<<"Transmitting acknowledgment signal\n";
             sleep(1);
-            write(AKCFDS2[1],readmsg,sizeof(readmsg));
+            write(ACKFDS2[1],readmsg,sizeof(readmsg));
         }
     }
     else if (n1 > 0 && n2 == 0)
@@ -104,14 +104,14 @@ int main()
         char readmsg[20];
         close(sndrFDS1[0]);close(sndrFDS1[1]);
         close(sndrFDS2[1]);
-        close(AKCFDS1[0]);
-        close(AKCFDS2[0]);close(AKCFDS2[1]);
+        close(ACKFDS1[0]);
+        close(ACKFDS2[0]);close(ACKFDS2[1]);
         while(true)
         {
             read(sndrFDS2[0],readmsg,sizeof(readmsg));
             cout<<"Receiver:"<<readmsg<<endl<<"Sending acknowledgment\n";
             sleep(1);
-            write(AKCFDS1[1],"+",sizeof("+"));
+            write(ACKFDS1[1],"+",sizeof("+"));
         }
     }
     return 0;
